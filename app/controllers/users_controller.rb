6663987beb02f43f409015d792a1a7eb9f9de48f
Eric Furrevig <efurrevig@gmail.com>
2023-01-08 16:48:07 -0500

    User new/create controller and view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,22 @@
 class UsersController < ApplicationController
+    def index
+        @users = User.all
+    end
+    
     def new
+        @user = User.new
     end
 
     def show
     end
 
-    def index
-    end
-
     def create
+        @user = User.new(user_params)
+        if @user.save
+            redirect_to users_url
+        else
+            render :new, status: :unprocessable_entity
+        end
     end
 
     def edit
@@ -22,5 +30,6 @@ class UsersController < ApplicationController
 
     private
         def user_params
+            params.require(:user).permit(:username, :password, :email)
         end
 end
